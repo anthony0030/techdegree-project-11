e1aa6923ee8a4cd013a4e9fe148c08afc6d00bff
Anthony Veaudry <anthonyveaudry@gmail.com>
2018-09-21 13:51:32 +0300

    convert images into componenants
    Signed-off-by: Anthony Veaudry <anthonyveaudry@gmail.com>

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 //Application Components
 import MainNavigation from './components/MainNavigation'
 import SearchForm from './components/SearchForm'
+import Image from './components/Image'
 
 class App extends Component {
   render() {
@@ -17,18 +18,10 @@ class App extends Component {
         <div className="photo-container">
           <h2>Results</h2>
           <ul>
-            <li>
-              <img src="https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg" alt="" />
-            </li>
-            <li>
-              <img src="https://farm5.staticflickr.com/4342/36338751244_316b6ee54b.jpg" alt="" />
-            </li>
-            <li>
-              <img src="https://farm5.staticflickr.com/4343/37175099045_0d3a249629.jpg" alt="" />
-            </li>
-            <li>
-              <img src="https://farm5.staticflickr.com/4425/36337012384_ba3365621e.jpg" alt="" />
-            </li>
+            <Image url="https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg"/>
+            <Image url="https://farm5.staticflickr.com/4342/36338751244_316b6ee54b.jpg"/>
+            <Image url="https://farm5.staticflickr.com/4343/37175099045_0d3a249629.jpg"/>
+            <Image url="https://farm5.staticflickr.com/4425/36337012384_ba3365621e.jpg"/>
             {/*<!-- Not Found -->*/}
             <li className="not-found">
               <h3>No Results Found</h3>
