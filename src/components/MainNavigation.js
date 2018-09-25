df99d18fb5a939690f09a07ccda33810dbbacaee
Anthony Veaudry <anthonyveaudry@gmail.com>
2018-09-25 12:27:03 +0300

    make the navlinks exact
    Signed-off-by: Anthony Veaudry <anthonyveaudry@gmail.com>

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -4,10 +4,10 @@ import {NavLink} from 'react-router-dom';
 const MainNavigation = props => (
   <nav className="main-nav">
     <ul>
-      <li><NavLink to="/cats">Cats</NavLink></li>
-      <li><NavLink to="/dogs">Dogs</NavLink></li>
-      <li><NavLink to="/computers">Computers</NavLink></li>
-      <li><NavLink to="/coffee">Coffee</NavLink></li>
+      <li><NavLink exact to="/cats">Cats</NavLink></li>
+      <li><NavLink exact to="/dogs">Dogs</NavLink></li>
+      <li><NavLink exact to="/computers">Computers</NavLink></li>
+      <li><NavLink exact to="/coffee">Coffee</NavLink></li>
     </ul>
   </nav>
 );
