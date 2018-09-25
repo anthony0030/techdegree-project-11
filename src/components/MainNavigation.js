6dab993582483ea3c02312f98bb8af2903cf8f37
Anthony Veaudry <anthonyveaudry@gmail.com>
2018-09-25 12:23:16 +0300

    add coffe link
    Signed-off-by: Anthony Veaudry <anthonyveaudry@gmail.com>

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -7,6 +7,7 @@ const MainNavigation = props => (
       <li><NavLink to="/cats">Cats</NavLink></li>
       <li><NavLink to="/dogs">Dogs</NavLink></li>
       <li><NavLink to="/computers">Computers</NavLink></li>
+      <li><NavLink to="/coffee">Coffee</NavLink></li>
     </ul>
   </nav>
 );
