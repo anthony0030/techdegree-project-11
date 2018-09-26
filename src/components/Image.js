6eec46c8deaae6be45d58a92f906ade670941e88
Anthony Veaudry <anthonyveaudry@gmail.com>
2018-09-26 13:39:18 +0300

    change the alt tags of the images to be the images title
    Signed-off-by: Anthony Veaudry <anthonyveaudry@gmail.com>

--- src/components/Image.js
+++ src/components/Image.js
@@ -5,7 +5,7 @@ function Image(props){
   return(
     <li>
       <a href={props.url} data-lightbox="search" data-title={props.title}>
-        <img src={props.url} title={props.title} alt="" />
+        <img src={props.url} title={props.title} alt={props.title} />
       </a>
     </li>
   )
