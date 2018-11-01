6da6ace284472840f5b8f58df8956b940edf0b52
Anthony Veaudry <anthonyveaudry@gmail.com>
2018-11-01 18:34:15 +0200

    add some spaces
    Signed-off-by: Anthony Veaudry <anthonyveaudry@gmail.com>

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 
 function MainNavigation(props) {
