1f905023a550b0139ba8382cfb16ecb0f3c73b4e
Anthony Veaudry <anthonyveaudry@gmail.com>
2018-11-05 12:20:45 +0200

    simplify not found component
    Signed-off-by: Anthony Veaudry <anthonyveaudry@gmail.com>

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -2,10 +2,11 @@ import React from "react";
 
 // returns a not found page
 function NotFound(props){
+  const {title} = props;
   return(
     <div className="not-found">
       <h3>No Results Found</h3>
-      <p>You search for <strong>{props.title}</strong> did not return any results. Please try again.</p>
+      <p>You search for <strong>{props}</strong> did not return any results. Please try again.</p>
     </div>
   );
 }
